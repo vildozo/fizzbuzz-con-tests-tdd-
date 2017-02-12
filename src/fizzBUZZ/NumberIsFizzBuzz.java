6c0f6ed5b2035078402321b1b918e0c3d1f2f1cd
vildozo <vildozo@outlook.com>
2017-02-12 00:02:28 -0400

    Se elimino numeros magicos para obtener el residuo

--- src/fizzBUZZ/NumberIsFizzBuzz.java
+++ src/fizzBUZZ/NumberIsFizzBuzz.java
@@ -2,7 +2,8 @@ package fizzBUZZ;
 
 public class NumberIsFizzBuzz {
 
-	
+	private static final int numeroFizz = 3;
+	private static final int numeroBuzz = 5;
 	
 	public boolean esMultiplo(int numerador, int denominador){
 		return (numerador % denominador == 0);
@@ -11,24 +12,24 @@ public class NumberIsFizzBuzz {
 	
 	public static void main(String[] args)
 	{
-		for(int i = 0; i < 100; i++)
+		for(int numeroContador = 0; numeroContador < 100; numeroContador++)
 		{
-			if (i % 3 == 0 && i % 5 == 0)
+			if (numeroContador % numeroFizz == 0 && numeroContador % numeroBuzz == 0)
 			{
 				System.out.println("FizzBuzz");                    
 			}
 			else 
-				if(i % 3 == 0)
+				if(numeroContador % numeroFizz == 0)
 				{
 					System.out.println("Fizz");
 				}
 				else 
-					if(i % 5 == 0)
+					if(numeroContador % numeroBuzz == 0)
 					{
 						System.out.println("Buzz");
 					}
 					else
-						System.out.println(i);
+						System.out.println(numeroContador);
         }
     }	
 }
